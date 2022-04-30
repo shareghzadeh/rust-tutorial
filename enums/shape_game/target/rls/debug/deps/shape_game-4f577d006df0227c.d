4e706aa8b773bc81b3b0fc300e5635df555493e1
HamidReza <59317713+shareghzade@users.noreply.github.com>
2022-04-30 19:47:14 +0430

    Delete enums/shape_game/target directory

--- enums/shape_game/target/rls/debug/deps/shape_game-4f577d006df0227c.d
+++
@@ -1,5 +0,0 @@
-/home/hamid/Programming/Rust/Tutorial/enums/shape_game/target/rls/debug/deps/shape_game-4f577d006df0227c.rmeta: src/main.rs
-
-/home/hamid/Programming/Rust/Tutorial/enums/shape_game/target/rls/debug/deps/shape_game-4f577d006df0227c.d: src/main.rs
-
-src/main.rs:
--- enums/shape_game/target/rls/debug/deps/shape_game-ba7933b2f244f3e3.d
+++
@@ -1,5 +0,0 @@
-/home/hamid/Programming/Rust/Tutorial/enums/shape_game/target/rls/debug/deps/shape_game-ba7933b2f244f3e3.rmeta: src/main.rs
-
-/home/hamid/Programming/Rust/Tutorial/enums/shape_game/target/rls/debug/deps/shape_game-ba7933b2f244f3e3.d: src/main.rs
-
-src/main.rs:
